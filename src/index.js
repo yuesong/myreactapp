9a8b430ad4fa14e3e92e0ae23e91fde58a2f6a7e
Yuesong Wang <yuesong.wang@oracle.com>
2022-04-30 21:17:30 -0400

    moved state to Game

--- src/index.js
+++ src/index.js
@@ -10,8 +10,37 @@ function Square(props) {
   );
 }
 
-class Board extends React.Component {
+function Board(props) {
+  let renderSquare = (i) => {
+    return (
+      <Square
+        value={props.squares[i]}
+        onClick={() => props.onClick(i)}
+      />);
+  };
 
+  return (
+    <div>
+      <div className="board-row">
+        {renderSquare(0)}
+        {renderSquare(1)}
+        {renderSquare(2)}
+      </div>
+      <div className="board-row">
+        {renderSquare(3)}
+        {renderSquare(4)}
+        {renderSquare(5)}
+      </div>
+      <div className="board-row">
+        {renderSquare(6)}
+        {renderSquare(7)}
+        {renderSquare(8)}
+      </div>
+    </div>
+  );
+}
+
+class Game extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
@@ -32,14 +61,6 @@ class Board extends React.Component {
     });
   }
 
-  renderSquare(i) {
-    return (
-      <Square
-        value={this.state.squares[i]}
-        onClick={() => this.handleClick(i)}
-      />);
-  }
-
   render() {
     const winner = calculateWinner(this.state.squares);
     let status;
@@ -48,39 +69,16 @@ class Board extends React.Component {
     } else {
       status = 'Next player: '+ (this.state.xIsNext ? 'X' : 'O');
     }
-
-    return (
-      <div>
-        <div className="status">{status}</div>
-        <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
-        </div>
-      </div>
-    );
-  }
-}
-
-class Game extends React.Component {
-  render() {
     return (
       <div className="game">
         <div className="game-board">
-          <Board />
+          <Board
+            squares={this.state.squares}
+            onClick={i => this.handleClick(i)}
+          />
         </div>
         <div className="game-info">
-          <div>{/* status */}</div>
+          <div className="status">{status}</div>
           <ol>{/* TODO */}</ol>
         </div>
       </div>
